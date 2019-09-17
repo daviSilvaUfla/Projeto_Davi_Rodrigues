17981e9b1fed5f05e63acfe5afacad376bbe0032
daviSilvaUfla <davi.silva@estudante.ufla.br>
2019-09-17 16:44:44 -0300

    Criaçao da classe C

---
+++ c.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class C {
+  private String C1;
+  private int C2;
+  
+//   gets e sets
+  public int getC1() {
+    return C1;
+  }
+  public String getC2() {
+    return C2;
+  }
+  public void setC1(int C1) {
+    this->C1 = C1;
+  }
+  public void setC2(int C2) {
+    this->C2 = C2;
+  }
+  
+//   métodos que imprime seu proprio nome
+  public void MC1() {
+    cout << "MC1" << endl;
+  }
+
+  public void MC2() {
+    cout << "MC2" << endl;
+  }
+};
