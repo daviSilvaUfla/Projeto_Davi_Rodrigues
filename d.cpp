e7daa9b9dc0859239b36bba5126f4ff95a1824d5
daviSilvaUfla <davi.silva@estudante.ufla.br>
2019-09-17 23:29:23 -0300

    criado class d

---
+++ d.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class D {
+    private int D1;
+    private float D2;
+    
+    public void MD1() {
+      cout << "MD1" << endl;
+    }
+    public void MD2() {
+      cout << "MD2" << endl;
+    }
+  };
